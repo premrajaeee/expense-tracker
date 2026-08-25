5c19e715c1e8b31fe6a2222d7f9630862f404b06
premrajaeee <premrajaeee@gmail.com>
2026-08-25 23:12:49 +0530

    Modified app.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "expense-tracker-v1";
+const CACHE_NAME = "expense-tracker-v2";
 
 const FILES_TO_CACHE = [
     "./",
@@ -12,6 +12,21 @@ self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames
+                        .filter(cacheName => cacheName !== CACHE_NAME)
+                        .map(cacheName => caches.delete(cacheName))
+                );
+            })
+            .then(() => self.clients.claim())
     );
 });
 
